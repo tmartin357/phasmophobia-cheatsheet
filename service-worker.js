4e2b07a4b96536244f086ab7fe46f265bf65977c
Taylor Martin <taylor.martin357@gmail.com>
2021-10-13 17:22:16 -0400

    Updates
    Build from e5fc3055bf94cde9dda6ae36fa45a38eb9f9af49

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/phasmophobia-cheatsheet/precache-manifest.a6e1b873427772cb3a24521e0309b5bf.js"
+  "/phasmophobia-cheatsheet/precache-manifest.af042c2f169f17f6de902ad47f2af7ef.js"
 );
 
 self.addEventListener('message', (event) => {
